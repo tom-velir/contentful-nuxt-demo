6dba81250552e3cfb83f26b8a146a989b2f92589
Tom Tillistrand <tom.tillistrand@velir.com>
2024-04-07 14:21:41 -0400

    Tweak ESLint and Stylelint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,9 @@
 module.exports = {
   root: true,
-  extends: ["@nuxt/eslint-config"],
+  extends: ["@nuxt/eslint-config", "prettier"],
   rules: {
     "vue/no-multiple-template-root": 0,
     "vue/singleline-html-element-content-newline": 0,
+    "vue/html-self-closing": 0,
   },
 };
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["/node_modules/water.css/out/dark.css"],
+  css: ["/node_modules/water.css/out/dark.css", "assets/styles/global.scss"],
 });
